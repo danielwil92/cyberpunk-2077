1e9f13153bf4e786dc4f83fc0c64841e474f75c8
danielwil92 <danielwilches.kamui@gmail.com>
2020-07-27 21:01:48 -0500

    Added initial structure for: Header, Logo and Button components.

---
+++ src/components/common/logo/logo.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './logo.scss';
+
+export default function Logo() {
+  return (
+    <img src="/cyberpunk-2077-logo.png" alt="Cyberpunk 2077 Logo" className="cyberpunk-logo" />
+  );
+}
