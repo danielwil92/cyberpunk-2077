731c4568fa75c758b84891e68651442b85ceea18
danielw92 <danielwilches.kamui@gmail.com>
2020-08-08 19:57:08 -0500

    [Component] | Section Wrapper
    * Added new Section Wrapper component to add new sections on Pages.
    * Removed page.scss in favor of new Section Wrapper Component.
    
    Bonus: Change hypens CSS to none on heading.scss due to unnexpected behavior.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../scss/all.scss';
 import Header from '../components/common/header/header';
+import SectionWrapper from '../components/layout/wrapper/section-wrapper';
 import Hero from '../content/hero/hero';
 import HeroContent from '../data/content/home/hero.json';
 import About from '../content/about/about';
@@ -11,20 +12,20 @@ export default function Home() {
     <div>
       <Header />
       <main>
-        <section className="wrapper wrapper--full-bleed-sm">
+        <SectionWrapper fullBleedSM>
           <Hero
             headline={HeroContent.headline}
             subheading={HeroContent.subheading}
             cta={HeroContent.cta}
             supportedPlatforms={HeroContent.supported_platforms}
           />
-        </section>
-        <section className="wrapper mod-spacer-6">
+        </SectionWrapper>
+        <SectionWrapper spacer={6}>
           <About
             headline={AboutContent.headline}
             body={AboutContent.body}
           />
-        </section>
+        </SectionWrapper>
       </main>
     </div>
   );
